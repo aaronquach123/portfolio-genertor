bc74804838e46b63c6736378d88503c1fa1cb03c
aaron <aaronquach12341@gmail.com>
2021-03-30 18:47:36 -0700

    finished html

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
 const inquirer = require("inquirer");
-//const fs = require("fs");
+const fs = require("fs");
 
-//const generatePage = require("./src/page-template.js")
-
-//const [name, github] = profileDataArgs;
-
-//const pageHTML = generatePage(name, github);
-
-//fs.writeFile("./index.html", pageHTML, err => {
-    //if (err) throw err;
-
-    //console.log("Portfolio comeplete! Check out index.html to see the output!")
-//});
+const generatePage = require("./src/page-template.js")
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -151,4 +141,11 @@ promptUser()
     .then(promptProject)
     .then(portfolioData => {
         console.log(portfolioData);
-    })
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw new Error(err);
+
+            console.log('Page created! Check out index.html in this directory to see it!');
+        });
+    });
